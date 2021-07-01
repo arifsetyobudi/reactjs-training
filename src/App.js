341bba5327600f4559822b13468b3f0a42bc313a
Arif Setyo <arifsetyo.budi@mitrais.com>
2021-07-01 20:42:58 +0700

    JSX part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
+import logo from './logo.svg';
+
 function App() {
+  const title = "React Training";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is jsx, not html. {title}</h1>
+      <table>
+        <tr>
+          <td style={{ color: "#ff0000", padding: "5px" }}>JSX inline style</td>
+        </tr>
+      </table>
     </div>
   );
 }
